37d26aab0fe8cc52b4ad29b7e1120081320a09a7
Magda Lorena Ramirez Calderon <106620164+Lorena2387@users.noreply.github.com>
2023-02-14 12:46:27 -0500

    app hotel

---
+++ lib/Screen/Theme/big_text.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class BigText extends StatelessWidget {
+  final Color? color;
+  final String text;
+  double size;
+  TextOverflow overFlow;
+  BigText(
+      {super.key,
+      this.color = Colors.white,
+      required this.text,
+      this.size = 18,
+      this.overFlow = TextOverflow.ellipsis});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      overflow: overFlow,
+      style: TextStyle(
+        fontFamily: 'Roboto',
+        color: color,
+      ),
+    );
+  }
+}
