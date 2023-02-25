2ed881fbf24e3b696b052b6874c86a2cb8c846df
Magda Lorena Ramirez Calderon <106620164+Lorena2387@users.noreply.github.com>
2023-02-24 20:28:58 -0500

    app hotel

--- lib/Screen/cabana_familiar.dart
+++ lib/Screen/cabana_familiar.dart
@@ -11,22 +11,16 @@ class CabanaFamiliar extends StatefulWidget {
 }
 
 class _CabanaFamiliarState extends State<CabanaFamiliar> {
-  List<String> images = [
-    'assets/images/cabanafam1.png',
-    'assets/images/cabanafam2.png',
-    'assets/images/cabanafam3.png',
-    'assets/images/cabanafam4.png'
-  ];
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-        body: CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          backgroundColor: AppTheme.primary,
-          actions: [],
-        ),
-      ],
-    ));
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: AppTheme.primary,
+          ),
+        ],
+      ),
+    );
   }
 }
